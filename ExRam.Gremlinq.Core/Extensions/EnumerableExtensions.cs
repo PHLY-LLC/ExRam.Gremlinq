79ce1d69cc21a005a5cc21ea9fdbdcf549aa6f27
Daniel C. Weber <dw@exram.de>
2018-12-20 19:12:20 +0100

    Abstract WorkaroundTINKERPOP_2112 into a Batch extension for reuse.

--- ExRam.Gremlinq.Core/Extensions/EnumerableExtensions.cs
+++ ExRam.Gremlinq.Core/Extensions/EnumerableExtensions.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections.Generic;
 using ExRam.Gremlinq.Core;
+using LanguageExt;
 
 namespace System.Linq
 {
     public static class EnumerableExtensions
     {
+        private sealed class PropertyStepComparer : IComparer<PropertyStep>
+        {
+            public static readonly PropertyStepComparer Instance = new PropertyStepComparer();
+
+            public int Compare(PropertyStep x, PropertyStep y)
+            {
+                return -(x?.Key is T).CompareTo(y?.Key is T);
+            }
+        }
+
         internal static IEnumerable<Step> HandleAnonymousQueries(this IEnumerable<Step> steps)
         {
             using (var e = steps.GetEnumerator())
@@ -24,10 +35,9 @@ namespace System.Linq
             }
         }
 
-        //https://issues.apache.org/jira/browse/TINKERPOP-2112.
-        internal static IEnumerable<Step> WorkaroundTINKERPOP_2112(this IEnumerable<Step> steps)
+        internal static IEnumerable<Either<Step, TStep[]>> Batch<TStep>(this IEnumerable<Step> steps) where TStep : Step
         {
-            var propertySteps = default(List<PropertyStep>);
+            var propertySteps = default(List<TStep>);
 
             using (var e = steps.GetEnumerator())
             {
@@ -35,33 +45,67 @@ namespace System.Linq
                 {
                     var hasNext = e.MoveNext();
 
-                    if (hasNext && e.Current is PropertyStep propertyStep)
+                    if (hasNext && e.Current is TStep firstStep)
                     {
-                        if (propertySteps == null)
-                            propertySteps = new List<PropertyStep>();
+                        if (propertySteps == null || propertySteps.Count == 0)
+                        {
+                            if (hasNext = e.MoveNext())
+                            {
+                                if (e.Current is TStep secondStep)
+                                {
+                                    if (propertySteps == null)
+                                        propertySteps = new List<TStep>();
 
-                        propertySteps.Add(propertyStep);
+                                    propertySteps.Add(firstStep);
+                                    propertySteps.Add(secondStep);
+                                }
+                                else
+                                {
+                                    yield return firstStep;
+                                    yield return e.Current;
+                                }
+                            }
+                            else
+                                yield return firstStep;
+                        }
+                        else
+                            propertySteps.Add(firstStep);
                     }
                     else
                     {
                         if (propertySteps != null && propertySteps.Count > 0)
                         {
-                            propertySteps.Sort((x, y) => -(x.Key is T).CompareTo(y.Key is T));
-
-                            foreach (var replayPropertyStep in propertySteps)
-                            {
-                                yield return replayPropertyStep;
-                            }
+                            yield return propertySteps.ToArray();
 
                             propertySteps.Clear();
                         }
 
-                        if (hasNext)    
+                        if (hasNext)
                             yield return e.Current;
                     }
 
                     if (!hasNext)
                         break;
+                }
+            }
+        }
+
+        //https://issues.apache.org/jira/browse/TINKERPOP-2112.
+        internal static IEnumerable<Step> WorkaroundTINKERPOP_2112(this IEnumerable<Step> steps)
+        {
+            foreach (var either in steps.Batch<PropertyStep>())
+            {
+                if (either.IsLeft)
+                    yield return (Step)either;
+                else
+                {
+                    var propertySteps = (PropertyStep[])either;
+                    Array.Sort(propertySteps, PropertyStepComparer.Instance);
+
+                    foreach (var replayPropertyStep in propertySteps)
+                    {
+                        yield return replayPropertyStep;
+                    }
                 }
             }
         }
